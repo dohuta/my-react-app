5dc2bfc5b6415a5e203512313830282f59e9041e
thuynh68 <thuynh68@dxc.com>
2021-03-16 21:11:32 +0700

    Update demo

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test("renders learn react link", () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("renders new text", () => {
+  render(<App />);
+  const dockerText = screen.getByText(/docker/i);
+  expect(dockerText).toBeInTheDocument();
+});
